ca9cf9e962c439cc37b1c93fd052a37de9982266
Jeffrey345678 <31826382+Jeffrey345678@users.noreply.github.com>
2020-11-08 10:46:51 -0700

    Add files via upload

---
+++ BinaryTreeNode.java
@@ -0,0 +1,16 @@
+package BinaryTree;
+
+public class BinaryTreeNode {
+	int IP;
+	String User;
+	BinaryTreeNode left;
+	BinaryTreeNode right;
+	
+	BinaryTreeNode(int IP, String User){
+		this.IP = IP;
+		this.User = User;
+		left = null;
+		right = null;
+	}
+	
+}
